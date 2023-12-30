36ada824f68bc8426bf19f667b625ffdde6d84e8
edward-tawa <tagumatawa@gmail.com>
2023-12-29 16:38:47 -0800

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """ script to start a web application """
-
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route('/', strict_slashes = False)
+
+@app.route('/', strict_slashes=False)
 def hello():
     """ returns Hello HBNB! """
+
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
