b6a2c3a03a3b8b74dc19d88c48341e067e40610f
edward-tawa <tagumatawa@gmail.com>
2023-12-30 02:16:14 -0800

    4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -6,7 +6,8 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-@app.route('/', strict_slashes = False)
+
+@app.route('/', strict_slashes=False)
 def hello():
     """ returns text """
     return 'Hello HBNB!'
@@ -42,4 +43,3 @@ def number_text(n):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
